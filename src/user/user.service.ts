d2b4a2a180ee2daddc296c89bd52b1ec21bdede1
시혁 박 <exon.indiv@gmail.com>
2022-04-09 17:02:13 +0900

    Feat:

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,42 +1,42 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { PrismaClient } from '@prisma/client'
+import { type } from 'os'
+
+type IUser = {
+    id: string
+    password: string
+}
 
 @Injectable()
 export class UserService {
     prisma = new PrismaClient()
-    async getUser(userId: string) {
-        const user = await this.prisma.user.findUnique({
+
+    async isUserExist(userId: string): Promise<false | IUser> {
+        const user: IUser = await this.prisma.user.findUnique({
             where: { id: userId },
         })
-        if (user) {
-            return {
-                id: user.id,
-                password: user.password,
-            }
-        }
-        return {
-            userId,
-        }
+        if (user) return user
+        else return false
+    }
+
+    async getUser(userId: string) {
+        const user: IUser | false = await this.isUserExist(userId)
+        if (user) return userId
+        else throw new NotFoundException(`${userId} is not exist`)
     }
 
     async createUser(userData) {
         const oldUser = await this.prisma.user.findUnique({
             where: { id: userData.id },
         })
-        if (oldUser) {
-            return {
-                statusCode: 500,
-                message: 'Already userId exist',
-            }
-        }
+        if (oldUser)
+            throw new NotFoundException(`Already Id ${oldUser.id} exist`)
         const newUser = await this.prisma.user.create({
             data: {
                 id: userData.id,
                 password: userData.password,
             },
         })
-        return {
-            ...newUser,
-        }
+        return { ...newUser }
     }
 }
